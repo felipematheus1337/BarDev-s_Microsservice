e4e13217bde45bfae7c8cac967abc1b20d5b2420
felipematheus1337 <lipehbr@gmail.com>
2023-02-05 17:10:34 -0300

    Criação do projeto!

---
+++ src/main/java/com/bardev/DevelopersBarApplication.java
@@ -0,0 +1,13 @@
+package com.bardev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bardev/DevelopersBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bardev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

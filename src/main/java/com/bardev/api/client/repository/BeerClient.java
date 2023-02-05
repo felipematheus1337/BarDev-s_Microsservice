5d54b26be19ecc73ccdb02f61c6ee2ae5b61db36
felipematheus1337 <lipehbr@gmail.com>
2023-02-05 17:28:21 -0300

    DTO BeerResponse criado!

--- src/main/java/com/bardev/api/client/repository/BeerClient.java
+++ src/main/java/com/bardev/api/client/repository/BeerClient.java
@@ -1,9 +1,12 @@
-package com.bardev.domain.repository;
+package com.bardev.api.client.repository;
 
 
+import com.bardev.api.client.response.BeerResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(value = "BeerClient", url = "https://api.punkapi.com/v2/beers")
 public interface BeerClient {
---
+++ src/main/java/com/bardev/api/client/response/BeerResponse.java
@@ -0,0 +1,12 @@
+package com.bardev.api.client.response;
+
+import lombok.Data;
+
+@Data
+public class BeerResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private Long abv;
+    private Long ibu;
+}

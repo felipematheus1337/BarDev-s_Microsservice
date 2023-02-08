244cbf43b07349313ce95a82697dd62d8b81785a
felipematheus1337 <lipehbr@gmail.com>
2023-02-08 16:02:38 -0300

    Collection Beer, criada!

---
+++ src/main/java/com/bardev/domain/entity/Beer.java
@@ -0,0 +1,35 @@
+package com.bardev.domain.entity;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Document(collection = "beer")
+public class Beer implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    private String name;
+
+
+    private String description;
+
+    private double price;
+
+    private AlcoholLevel alcoholLevel;
+
+    private ColorLevel colorLevel;
+
+    
+
+
+
+
+}
